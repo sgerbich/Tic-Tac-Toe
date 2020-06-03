1c7113829aeb1f430af2a78f62dddc017c2165f9
unknown <sdgerbich@gmail.com>
2020-06-03 11:13:04 -0400

    tests pass now

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 
-public class TicTacToe
-{
-	// Instance Variables
-	private char[][] board;
+public class TicTacToe {
+    // Instance Variables
+    private char[][] board;
     private int turns;
     static Scanner input = new Scanner(System.in);
 
@@ -20,8 +19,30 @@ public class TicTacToe
     // Accessor Methods
 
     public boolean isWinner(char p) {
+        // check rows
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == p && board[i][1]==board[i][0] && board[i][1]==board[i][2]) {
+                return true;
+            }
+        }
+        // check cols
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] == p && board[1][i]==board[0][i] && board[1][i]==board[2][i]) {
+                return true;
+            }
+        }
+        // check diagonal
+        if(board[0][0] == p && board[1][1]==board[0][0] && board[2][2]==board[1][1]){
+            return true;
+        }
+
+        if(board[2][0] == p && board[1][1]==board[2][0] && board[0][2]==board[1][1]){
+            return true;
+        }
         return false;
     }
+
+    
 
     public boolean isFull() {
         for (int i = 0; i < 3; i++) {
@@ -35,6 +56,9 @@ public class TicTacToe
     }
 
     public boolean isCat() {
+        if (turns >= 9 && !isWinner('X')&& !isWinner('O')){
+            return true;
+        }
         return false;
     }
 
@@ -67,9 +91,10 @@ public class TicTacToe
 
     // Modifiers
     public void playMove(char p, int r, int c) {
-        if(board[r][c] == ' '){
-            board[r][c]= p;
-        }else{
+        if (board[r][c] == ' ') {
+            board[r][c] = p;
+            turns++;
+        } else {
             System.out.println("sorry you cant go here");
         }
     }
@@ -77,14 +102,28 @@ public class TicTacToe
     public static void main(String[] args) {
         TicTacToe ttt = new TicTacToe();
         ttt.displayBoard();
-        while (ttt.numTurns() < 9) {
+        char p = 'X';
+        while (!ttt.isCat() && !ttt.isWinner(p)) {
             // System.out.println( + " Choose your location (row, column):");
+
+            if (ttt.numTurns() % 2 != 0) {
+                p = 'O';
+            } else {
+                p = 'X';
+            }
             int row = input.nextInt();
-            int col= input.nextInt();
-            ttt.playMove('X', row, col);
+            int col = input.nextInt();
+            ttt.playMove(p, row, col);
             ttt.displayBoard();
+
+        }
+
+        if (ttt.isWinner(p)){
+            System.out.println("Congratulations "+ p+ " you win!");
+        }
+
+        if (ttt.isCat()){
+            System.out.println("Its a tie");
         }
     }
-
-
 }
